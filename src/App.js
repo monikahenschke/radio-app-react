d1ac74a85987fe5415732d6dcce41b8db1c0b966
monikahenschke <48621957+monikahenschke@users.noreply.github.com>
2022-01-11 11:00:09 +0100

    RD-13 button component (#7)
    * button component
    
    * button component- v2
    
    * button component- v3
    
    * adjustments for Button component
    
    * classnames installing, some changes in Button component
    
    * adding an error to the button component
    
    Co-authored-by: Monika <monika.henschke@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Button from './components/Button';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">Hello!</header>
+      <Button
+        type="button"
+        children="Add new radio station"
+        onClick={clickPlay}
+        size="big"
+      />
     </div>
   );
+}
+
+function clickPlay() {
+  console.log('play');
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import propTypes, { func } from 'prop-types';
+import styles from './Button.module.scss';
+import cx from 'classnames';
+
+const Button = ({
+  iconOnly,
+  size,
+  icon: IconComponent,
+  children,
+  ...props
+}) => {
+  const iconClass = iconOnly && IconComponent ? styles.icon : undefined;
+  const sizeClass = size && styles[size];
+
+  if ((iconOnly || IconComponent) && (!iconOnly || !IconComponent)) {
+    const errorText =
+      'You cannot use iconOnly and icon separately. You have to pass these props together.';
+    throw new Error(errorText);
+  }
+  return (
+    <button className={cx(styles.button, sizeClass, iconClass)} {...props}>
+      {iconOnly && IconComponent ? <IconComponent /> : children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: propTypes.node.isRequired,
+  onClick: func.isRequired,
+};
+
+export default Button;
