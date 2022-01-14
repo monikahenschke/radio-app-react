fcdbf3e1294fdd8081f950fb70850f2ffd520940
Monika <monika.henschke@gmail.com>
2022-01-14 13:32:20 +0100

    Audio managing functions

---
+++ src/utils/audio-manager.js
@@ -0,0 +1,34 @@
+import { func } from 'prop-types';
+
+class AudioManager {
+  constructor(url) {
+    this.setNewStation(url);
+  }
+
+  play() {
+    this.audio.play();
+  }
+
+  pause() {
+    this.audio.pause();
+  }
+
+  select(url) {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+    this.setNewStation(url);
+    this.play();
+  }
+
+  setNewStation(url) {
+    this.audio = new Audio(url);
+  }
+}
+
+let audioManagerInstance = null;
+
+const getInstance = (url) => {
+  return (audioManagerInstance ??= new AudioManager(url));
+};
+
+export default getInstance;
