74ffff07b4b2c5f9135d908080cd5f48e5be15a6
Monika <monika.henschke@gmail.com>
2022-01-21 19:49:30 +0100

    stationslist component

---
+++ src/components/StationsList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from './Button';
+
+export const StationsList = ({ stations, handleSelect, ...props }) => {
+  console.log(stations);
+  return (
+    <ul>
+      {stations.map((station) => (
+        <li className="station" key={station.id}>
+          <Button
+            onClick={() => handleSelect(station.link)}
+            children={station.name}
+          ></Button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default StationsList;
