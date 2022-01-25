f3dfa381f4c868a4b0768ec0761dcfd8a334a82e
monikahenschke <48621957+monikahenschke@users.noreply.github.com>
2022-01-25 14:20:19 +0100

    RD-16 StationsList component (#12)
    * stationslist component
    
    * console log removing
    
    * using state to keep radio stations array
    
    Co-authored-by: Monika <monika.henschke@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import Button from './components/Button';
+import StationsList from './components/StationsList';
 import getAudioManager from './utils/audio-manager';
 const hardcodedRadioUrl = 'http://17573.live.streamtheworld.com/WCTKFMAAC.aac';
 const hardcodedRadioUrl1 = 'http://icepool.silvacast.com/COUNTRY108.mp3';
+const radioStationsArray = [
+  {
+    id: 'cat-country',
+    name: 'Cat County 98.1',
+    link: 'http://17573.live.streamtheworld.com/WCTKFMAAC.aac',
+  },
+  {
+    id: 'americas-country',
+    name: "America's Country",
+    link: 'https://ais-sa2.cdnstream1.com/1976_128.mp3',
+  },
+  {
+    id: 'ktwb-big-country',
+    name: 'KTWB Big Country 92.5',
+    link: 'https://14083.live.streamtheworld.com/KTWBFMAAC.aac',
+  },
+  {
+    id: 'country-208',
+    name: 'Country 108',
+    link: 'http://icepool.silvacast.com/COUNTRY108.mp3',
+  },
+  {
+    id: 'kickin-country',
+    name: "Kickin' Country 181.fm",
+    link: 'https://listen.181fm.com/181-kickincountry_128k.mp3',
+  },
+];
 
 const App = () => {
+  const [radioStationsState, setRadioStationsState] =
+    useState(radioStationsArray);
+
   const audioManagerInstance = getAudioManager(hardcodedRadioUrl);
 
   const handlePlay = () => {
@@ -14,13 +46,14 @@ const App = () => {
     audioManagerInstance.pause();
   };
 
-  const handleSelect = () => {
-    audioManagerInstance.select(hardcodedRadioUrl1);
+  const handleSelect = (selectedStation) => {
+    audioManagerInstance.select(selectedStation);
   };
 
   return (
     <div className="App">
       <header className="App-header">Hello!</header>
+      <StationsList stations={radioStationsState} handleSelect={handleSelect} />
       <Button type="button" children="Play" onClick={handlePlay} size="big" />
       <Button type="button" children="Pause" onClick={handlePause} size="big" />
       <Button type="button" children="Station X " onClick={handleSelect} />
