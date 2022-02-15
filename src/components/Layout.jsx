5520f20e85d085b1de23c9deee727aa94bc89e93
monikahenschke <48621957+monikahenschke@users.noreply.github.com>
2022-02-15 21:43:07 +0100

    RD-21 layout component tests (#18)
    * creating a general wrapper for all of things in the project
    
    * tests of layout component
    
    * structure of the project
    
    * tests of layout component
    
    Co-authored-by: Monika <monika.henschke@gmail.com>
    Co-authored-by: Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import styles from './Layout.module.scss';
 
 export const Layout = ({ children }) => {
   return (
-    <div className={styles.Layout}>
+    <div data-testid="layout" className={styles.Layout}>
       <div className={styles.topBar}>Currently playing</div>
       <div className={styles.content}>{children}</div>
       <div className={styles.bottomBar}>Add new</div>
