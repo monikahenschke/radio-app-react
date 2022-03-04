3f5a97e89dd45f35678fafb4117a753cab44b1e5
monikahenschke <48621957+monikahenschke@users.noreply.github.com>
2022-03-04 10:49:41 +0100

    RD-23 using local storage (#21)
    * StationsListItem component creating
    
    * StationListItem Component - fix
    
    * RD-23 set radio stations to localStorage
    
    Co-authored-by: Monika <monika.henschke@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Cookies from 'js-cookie';
 import { Layout } from './components/Layout';
 import Content from './containers/Content';
+import { setBasicRadioStationsToLS } from './utils/setBasicRadioStations';
 
 function App() {
+  if (!Cookies.get('visited')) {
+    Cookies.set('visited', true);
+    // set basic radio stations in local storage on first visit
+    setBasicRadioStationsToLS();
+  }
+
   return (
     <Layout>
       <Content />
--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -6,34 +6,6 @@ import getAudioManager from '../utils/audio-manager';
 import StationsListItem from '../components/StationsListItem';
 const hardcodedRadioUrl = 'http://17573.live.streamtheworld.com/WCTKFMAAC.aac';
 
-const radioStationsArray = [
-  {
-    id: 'cat-country',
-    name: 'Cat County 98.1',
-    link: 'http://17573.live.streamtheworld.com/WCTKFMAAC.aac',
-  },
-  {
-    id: 'americas-country',
-    name: "America's Country",
-    link: 'https://ais-sa2.cdnstream1.com/1976_128.mp3',
-  },
-  {
-    id: 'ktwb-big-country',
-    name: 'KTWB Big Country 92.5',
-    link: 'https://14083.live.streamtheworld.com/KTWBFMAAC.aac',
-  },
-  {
-    id: 'country-208',
-    name: 'Country 108',
-    link: 'http://icepool.silvacast.com/COUNTRY108.mp3',
-  },
-  {
-    id: 'kickin-country',
-    name: "Kickin' Country 181.fm",
-    link: 'https://listen.181fm.com/181-kickincountry_128k.mp3',
-  },
-];
-
 const audioManagerInstance = getAudioManager(hardcodedRadioUrl);
 
 const handlePlay = () => {
@@ -49,13 +21,12 @@ const handleSelect = (selectedStation) => {
 };
 
 const Content = (props) => {
-  const [radioStationsState, setRadioStationsState] =
-    useState(radioStationsArray);
+  const radioStationsLS = JSON.parse(localStorage.getItem('stations'));
 
   return (
     <div data-testid="content" className={styles.Content}>
       <StationsList handleSelect={handleSelect}>
-        {radioStationsState.map((station, i) => (
+        {radioStationsLS.map((station, i) => (
           <StationsListItem
             key={i}
             title={station.name}
