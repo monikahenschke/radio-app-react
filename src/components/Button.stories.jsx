44711d9d672259e4f6392d719ae8ba91169c16ac
monikahenschke <48621957+monikahenschke@users.noreply.github.com>
2022-03-04 10:41:41 +0100

    RD-19 Storybook (#15)
    * storybook installing
    
    * first stories - storybook
    
    * removing of unnecessary stuff from storybook
    
    * optimization of the Button story
    
    Co-authored-by: Monika <monika.henschke@gmail.com>

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from './Button';
+import { ReactComponent as IconComponent } from '../images/play-large.svg';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    iconOnly: {
+      defaultValue: false,
+    },
+    children: {
+      defaultValue: 'Button',
+    },
+    size: {
+      options: ['small', 'big', 'default'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  onClick: undefined,
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  onClick: undefined,
+  size: 'big',
+  icon: IconComponent,
+  iconOnly: true,
+};
